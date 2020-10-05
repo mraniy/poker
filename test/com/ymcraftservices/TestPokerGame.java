d86d60bd306dad5fde6bde8b0c613b35cd7c70e6
younes <mraniy@gmail.com>
2020-10-05 12:51:25 +0200

    to be replaced next by Bdds!

--- test/com/ymcraftservices/TestPokerGame.java
+++ test/com/ymcraftservices/TestPokerGame.java
@@ -1,7 +1,9 @@
 package com.ymcraftservices;
 
 import com.ymcraftservices.model.*;
+import com.ymcraftservices.predicates.DoublePairPredicate;
 import com.ymcraftservices.service.PokerGame;
+import org.hamcrest.core.Is;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -17,8 +19,11 @@ public class TestPokerGame {
     private Player joe;
     private Player brian;
 
-    @BeforeEach
-    public void beforeEach() {
+
+
+    @Test
+    public void should_return_brian_as_winner_as_he_has_the_best_hand() {
+        // given in the setup
         Card flop1 = new Card(LabelCard.CARREAU, NumberCard.CINQ);
         Card flop2 = new Card(LabelCard.CARREAU, NumberCard.AS);
         Card flop3 = new Card(LabelCard.TREFLE, NumberCard.TROIS);
@@ -34,19 +39,78 @@ public class TestPokerGame {
         List<Card> samCards = Arrays.asList(samCard1, samCard2);
         List<Card> brianCards = Arrays.asList(brianCard1, briancard2);
         List<Card> joeCards = Arrays.asList(joeCard1, joeCard2);
-        Arrays.asList(joeCard1,joeCard2);
         sam = new Player("jackson","samuel",500,new Hand(potCards,samCards));
         joe = new Player("big","joe",600,new Hand(potCards,joeCards));
         brian = new Player("adams","brian",700,new Hand(potCards,brianCards));
-    }
-
-    @Test
-    public void should_return_brian_as_winner_as_he_has_the_best_hand() {
-        // given in the setup
         // when
         PokerGame pokerGame = new PokerGame();
         List<Player> winners = pokerGame.getWinner(Arrays.asList(joe, sam, brian));
         // assertThat
+        assertThat(winners.size(), is(1));
         assertThat(winners.get(0).getFirstName() , is(joe.getFirstName()));
+    }
+
+    @Test
+    public void should_return_brian_as_winner_as_he_has_the_best_double_pair() {
+        // given
+        Card flop1 = DataFactory.getCard(LabelCard.PIQUE, NumberCard.AS);
+        Card flop2 = DataFactory.getCard(LabelCard.CARREAU, NumberCard.VALLEE);
+        Card flop3 = DataFactory.getCard(LabelCard.TREFLE, NumberCard.SEPT);
+        Card turn = DataFactory.getCard(LabelCard.COEUR, NumberCard.SEPT);
+        Card river = DataFactory.getCard(LabelCard.CARREAU, NumberCard.DIX);
+
+        Card samCard1 = DataFactory.getCard(LabelCard.COEUR, NumberCard.NEUF);
+        Card samCard2 = DataFactory.getCard(LabelCard.CARREAU, NumberCard.AS);
+
+        Card brianCard1 = DataFactory.getCard(LabelCard.TREFLE, NumberCard.AS);
+        Card briancard2 = DataFactory.getCard(LabelCard.CARREAU, NumberCard.KING);
+
+        List<Card> brianCards = Arrays.asList(brianCard1, briancard2);
+        List<Card> samCards = Arrays.asList(samCard1, samCard2);
+
+        List<Card> potCards = Arrays.asList(flop1, flop2, flop3, turn, river);
+        sam = new Player("jackson","samuel",500,new Hand(potCards,samCards));
+        brian = new Player("adams","brian",700,new Hand(potCards,brianCards));
+
+        // when
+        PokerGame pokerGame = new PokerGame();
+        List<Player> winners = pokerGame.getWinner(Arrays.asList(sam, brian));
+        // assertThat
+        assertThat(winners.size(), is(1));
+        assertThat(winners.get(0).getFirstName() , is(brian.getFirstName()));
+    }
+
+    @Test
+    public void should_return_brian_and_joe_are_winners() {
+        // given in the setup
+        Card flop1 = new Card(LabelCard.CARREAU, NumberCard.CINQ);
+        Card flop2 = new Card(LabelCard.COEUR, NumberCard.AS);
+        Card flop3 = new Card(LabelCard.TREFLE, NumberCard.TROIS);
+        Card turn = new Card(LabelCard.CARREAU, NumberCard.DEUX);
+        Card river = new Card(LabelCard.COEUR, NumberCard.VALLEE);
+
+        Card samCard1 = new Card(LabelCard.PIQUE, NumberCard.DEUX);
+        Card samCard2 = new Card(LabelCard.CARREAU, NumberCard.DAME);
+
+        Card brianCard1 = new Card(LabelCard.COEUR, NumberCard.QUATRE);
+        Card briancard2 = new Card(LabelCard.PIQUE, NumberCard.TROIS);
+
+        Card joeCard1 = new Card(LabelCard.CARREAU, NumberCard.SEPT);
+        Card joeCard2 = new Card(LabelCard.CARREAU, NumberCard.QUATRE);
+
+        List<Card> potCards = Arrays.asList(flop1, flop2, flop3, turn, river);
+        List<Card> samCards = Arrays.asList(samCard1, samCard2);
+        List<Card> brianCards = Arrays.asList(brianCard1, briancard2);
+        List<Card> joeCards = Arrays.asList(joeCard1, joeCard2);
+        Arrays.asList(joeCard1,joeCard2);
+        sam = new Player("jackson","samuel",500,new Hand(potCards,samCards));
+        joe = new Player("big","joe",600,new Hand(potCards,joeCards));
+        brian = new Player("adams","brian",700,new Hand(potCards,brianCards));
+        // when
+        PokerGame pokerGame = new PokerGame();
+        List<Player> winners = pokerGame.getWinner(Arrays.asList(joe, sam, brian));
+        // assertThat
+        assertThat(winners.size(), is(2));
+        assertThat(winners.containsAll(Arrays.asList(brian, joe)) , is(true));
     }
 }
